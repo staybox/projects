ed0448bf02581aa50a26b266d8a2485343d9bbe1
Allexeyv <podarismile@gmail.com>
2016-08-26 15:27:57 +0400

    decorators

---
+++ training.py
@@ -0,0 +1,27 @@
+# coding: utf8
+from __future__ import print_function
+
+"""
+когда не нужно изменять функцию, используем декораторы
+"""
+
+def make_bold(fn):
+    def wrapped():
+        return '<b>' + fn() + '</b>'
+    return wrapped
+
+
+def make_italic(fn):
+    def wrapped():
+        return '<i>' + fn() + '</i>'
+    return wrapped
+
+
+@make_italic
+@make_bold
+def try_decorators():
+    return 'hello world'
+
+
+if __name__ == '__main__':
+    print(try_decorators())
