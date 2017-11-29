1655e72cf65639655c31f2aa4f023f2016d4c73d
Allexeyv <podarismile@gmail.com>
2017-11-30 01:04:18 +0300

    Update diskspace.py

--- sand/diskspace.py
+++ sand/diskspace.py
@@ -4,40 +4,40 @@ import argparse
 
 
 def find_sizes(args):
-  logicaldisks = args.logicaldisks
-  partitions = args.partitions
+  logicaldisk = args.logicaldisk
+  partition = args.partition
   platf = sys.platform
   cmd = ''
   if 'linux' in platf:
-      if logicaldisks != '':
-          cmd = 'lsblk -io --bytes KNAME,SIZE'
-      elif partitions != '':
-          cmd = 'lsblk -io --bytes KNAME,SIZE'
-      #subprocess.call(cmd, shell=True)
+      if logicaldisk != '':
+          cmd = 'lsblk -d -io KNAME,SIZE -e 1,11'
+      elif partition != '':
+          cmd = 'lsblk -io KNAME,SIZE -e 1,11 | grep -E "^sda[[:digit:]].|KNAME"'
+
   elif 'win' in platf:
-      if logicaldisks != '':
+      if logicaldisk != '':
           cmd = 'wmic logicaldisk get size,name'
-      elif partitions != '':
+      elif partition != '':
           cmd = 'wmic partition get size,name'
       else:
-          raise Exception('choose please one of the parameteres: --logicaldisks=1 or --partitions=1')
-      result = []
-      process = subprocess.Popen(cmd,
+          raise Exception('choose please one of the parameteres: --logicaldisk=1 or --partition=1')
+  result = []
+  process = subprocess.Popen(cmd,
                                shell=True,
                                stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE)
-      for line in process.stdout:
-          result.append(line)
-      errcode = process.returncode
-      for line in result:
-          print(line)
-      if errcode is not None:
-          raise Exception('cmd %s failed, see above for details', cmd)
+  for line in process.stdout:
+      result.append(line)
+  errcode = process.returncode
+  for line in result:
+      print(line)
+  if errcode is not None:
+      raise Exception('cmd %s failed, see above for details', cmd)
       
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--partitions', required=False, default='', help='to see partitions sizes')
-    parser.add_argument('--logicaldisks', required=False, default='', help='to see logicaldisks sizes')
+    parser.add_argument('--partition', required=False, default='', help='to see partition sizes')
+    parser.add_argument('--logicaldisk', required=False, default='', help='to see logicaldisk sizes')
     args = parser.parse_args()
     find_sizes(args)
